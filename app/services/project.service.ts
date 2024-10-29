19f4a8d310a629d4076c9adf99a1a205e506b84f
Ojaghi <Ojaghi.works@gmail.com>
2024-10-29 01:01:06 +0100

    feat(ProjectCRUD): add project crud

---
+++ app/services/project.service.ts
@@ -0,0 +1,27 @@
+/**
+ * Project Services
+ */
+
+import projectModel from "../models/project.model";
+
+// Types
+import  { ProjectTypes, ProjectIdType } from '../types/project'
+const projectService = {
+    createproject: async (project: ProjectTypes) =>{
+        return await projectModel.createproject(project)
+    },
+    updateproject: async (id: ProjectIdType, project: ProjectTypes) => {
+        return await projectModel.updateproject(id, project)
+    },
+    getAllprojects: async() => {
+        return await projectModel.getAllprojects()
+    },
+    getproject: async(id: ProjectIdType) => {
+        return await projectModel.getproject(id)
+    },
+    removeproject: async(id: ProjectIdType) => {
+        return await projectModel.removeproject(id)
+    }
+}
+
+export default projectService
---
+++ app/types/project.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Project Types
+ */
+
+export interface ProjectTypes {
+    projectName: string
+    budget: string
+    deadline: Date
+    id?: string
+}
+
+export type ProjectIdType = Pick<ProjectTypes, 'id'>
